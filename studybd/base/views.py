3e5f7a411e18a70214e3297dadc8ace5783ca7bd
anuj8848 <anujgiri8848@gmail.com>
2024-04-13 22:50:10 +0545

    content of db successfully rendered in specific page

--- studybd/base/views.py
+++ studybd/base/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .models import Room
-rooms=[
-    {  'id':1,'name':'lets learn python'},
-    {  'id':2,'name':'Design with me'},
-    {  'id':3,'name':'Frontend developers'},
 
-]
+# rooms=[
+#     {  'id':1,'name':'lets learn python'},
+#     {  'id':2,'name':'Design with me'},
+#     {  'id':3,'name':'Frontend developers'},
+
+# ]
 
 # Create your views here.
 def home(request):
@@ -13,10 +14,7 @@ def home(request):
     contexts={'rooms':rooms}
     return render(request,'base/home.html',contexts)
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
     contexts={'room':room}
     return render(request,'base/room.html',contexts)
 
